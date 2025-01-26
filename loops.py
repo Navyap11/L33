c6a30a98ba01b4f3026eb61acb79c833f6a616a5
navyap11 <ashwin.p.parmar@gmail.com>
2025-01-26 19:32:33 +0100

    f

---
+++ loops.py
@@ -0,0 +1,7 @@
+n= int(input("Enter a number till you want to stop the loop: "))
+sum= 0
+i= 1
+while i<=n:
+    sum= sum+i
+    i= i+1
+print("the sum of repeating the numbers ",n,"times is: ",sum)
